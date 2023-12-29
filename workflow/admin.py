f44e0def858d6dfbcee7193a568db10e1a1368d4
Victor Silva <pedrotivictor712@gmail.com>
2023-12-29 11:27:49 -0400

    inicial

--- workflow/admin.py
+++ workflow/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from workflow import models
+from workflow.models import models
 
 @admin.register(models.Pessoa)
 class WorkflowAdmin(admin.ModelAdmin):
@@ -11,5 +11,34 @@ class WorkflowAdmin(admin.ModelAdmin):
     ...
 
 @admin.register(models.Funcionario)
+class WorkflowAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(models.Equipamento)
+class WorkflowAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(models.Historico)
+class WorkflowAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(models.Setor)
+class WorkflowAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(models.Cargo_funcionario)
+class WorkflowAdmin(admin.ModelAdmin):
+    ...
+@admin.register(models.Servico)
+class WorkflowAdmin(admin.ModelAdmin):
+    ...
+@admin.register(models.Produto)
+class WorkflowAdmin(admin.ModelAdmin):
+    ...  
+@admin.register(models.Fornecedor)
+class WorkflowAdmin(admin.ModelAdmin):
+    ...
+@admin.register(models.Orcamento)
 class WorkflowAdmin(admin.ModelAdmin):
     ...
